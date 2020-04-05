653960a80d0a93f0f6aa693ab7899b442360d95e
Nitin Jain <jainnitin856@gmail.com>
2020-04-05 10:21:43 +0530

    added clache option

--- dataset.py
+++ dataset.py
@@ -13,11 +13,25 @@ from torch.utils.data import Dataset
 
 # In[3]:
 
+import cv2
+def clache(img_path):
+    clipLimit=2.0 
+    tileGridSize=(4, 4)  
+    bgr = cv2.imread(img_path)
+    lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=tileGridSize)
+    lab_planes[0] = clahe.apply(lab_planes[0]) 
+    lab = cv2.merge(lab_planes)
+    rgb = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+    return rgb
+
 
 class COVID19(Dataset):
-    def __init__(self, df, tf):
+    def __init__(self, df, tf, cl):
         self.df = df
         self.tf = tf
+        self.cl = cl
         
     def __len__(self):
         return len(self.df)
@@ -26,7 +40,10 @@ class COVID19(Dataset):
         _, img_path, labels = self.df.iloc[idx]
         
         try:
-            image = Image.open(img_path)
+            if self.cl:
+                image = Image.fromarray(clache(img_path))
+            else:
+                image = Image.open(img_path)
             if image.mode != 'RGB':
                 image = image.convert("RGB")
             
